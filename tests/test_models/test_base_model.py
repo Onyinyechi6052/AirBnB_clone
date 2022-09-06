63624795824117bfb34b1ec8edb10f8aac6f9f74
Onyinyechi6052 <101346795+Onyinyechi6052@users.noreply.github.com>
2022-09-06 13:11:43 +0100

    Create test_base_model.py

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,133 @@
+#!/usr/bin/python3
+"""
+A module that contains the test suite for the BaseModel class
+"""
+import unittest
+from time import sleep
+import os
+from datetime import datetime
+from uuid import uuid4
+
+import models
+from models.base_model import BaseModel
+
+
+class TestBaseModel(unittest.TestCase):
+    """
+    The test suite for models.base_model.BaseModel
+    """
+
+    def test_if_BaseModel_instance_has_id(self):
+        """
+        Checks that instance has an id assigned on initialization
+        """
+        b = BaseModel()
+        self.assertTrue(hasattr(b, "id"))
+
+    def test_str_representation(self):
+        """
+        Checks if the string representation is appropriate
+        """
+        b = BaseModel()
+        self.assertEqual(str(b),
+                         "[BaseModel] ({}) {}".format(b.id, b.__dict__))
+
+    def test_ids_is_unique(self):
+        """
+        Checks if id is generated randomly and uniquely
+        """
+        b1 = BaseModel()
+        b2 = BaseModel()
+        self.assertNotEqual(b1.id, b2.id)
+
+    def test_type_of_id_is_str(self):
+        """
+        Checks that id generated is a str object
+        """
+        b = BaseModel()
+        self.assertTrue(type(b.id) is str)
+
+    def test_created_at_is_datetime(self):
+        """
+        Checks that the attribute 'created_at' is a datetime object
+        """
+        b = BaseModel()
+        self.assertTrue(type(b.created_at) is datetime)
+
+    def test_updated_at_is_datetime(self):
+        """
+        Checks that the attribute 'updated_at' is a datetime object
+        """
+        b = BaseModel()
+        self.assertTrue(type(b.updated_at) is datetime)
+
+    def test_two_models_different_created_at(self):
+        """
+        Checks that the attribute 'created_at' of 2 models are different
+        """
+        b1 = BaseModel()
+        sleep(0.02)
+        b2 = BaseModel()
+        sleep(0.02)
+        self.assertLess(b1.created_at, b2.created_at)
+
+    def test_args_unused(self):
+        """
+        Checks that the attribute 'args' is not used.
+        """
+        b = BaseModel(None)
+        self.assertNotIn(None, b.__dict__.values())
+
+    def test_that_created_at_equals_updated_at_initially(self):
+        """
+        Checks that create_at == updated_at at initialization
+        """
+        b = BaseModel()
+        self.assertEqual(b.created_at, b.updated_at)
+
+    def test_that_save_func_update_update_at_attr(self):
+        """
+        Checks that save() method updates the updated_at attribute
+        """
+        b = BaseModel()
+        b.save()
+        self.assertNotEqual(b.created_at, b.updated_at)
+        self.assertGreater(b.updated_at.microsecond,
+                           b.created_at.microsecond)
+
+    def test_if_to_dict_returns_dict(self):
+        """
+        Checks if BaseModel.to_dict() returns a dict object
+        """
+        b = BaseModel()
+        self.assertTrue(type(b.to_dict()) is dict)
+
+    def test_if_to_dict_returns_class_dunder_method(self):
+        """
+        Checks if BaseModel.to_dict() contains __class__
+        """
+        b = BaseModel()
+        self.assertTrue("__class__" in b.to_dict())
+
+    def test_that_created_at_returned_by_to_dict_is_an_iso_string(self):
+        """
+        Checks that created_at is stored as a str obj in ISO format
+        """
+        b = BaseModel()
+        self.assertEqual(b.to_dict()["created_at"], b.created_at.isoformat())
+
+    def test_that_updated_at_returned_by_to_dict_is_an_iso_string(self):
+        """
+        Checks that updated_at is stored as a str obj in ISO format
+        """
+        b = BaseModel()
+        self.assertEqual(b.to_dict()["updated_at"], b.updated_at.isoformat())
+
+    def test_if_to_dict_returns_the_accurate_number_of_keys(self):
+        """
+        Checks that to_dict() returns the expected number of keys/values
+        """
+        b = BaseModel()
+        partial_expectation = {k: v for k, v in b.__dict__.items()
+                               if not k.startswith("_")}
+        self.assertEqual(len(b.to
